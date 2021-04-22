fcd3a3a14cf5d20eb0ca17f00daf4ce002596e5d
Chris Osborn <fozztexx@fozztexx.com>
2021-04-22 10:49:46 -0700

    Added license and README.

---
+++ README.md
@@ -0,0 +1,17 @@
+This is a program to emulate a PS/2 keyboard and PS/2 mouse so that a
+Raspberry Pi can act as the keyboard and mouse of another
+computer. Mostly for use with a video capture device so that a
+Raspberry Pi can act as a IP KVM.
+
+There is no video capture stuff here, this is **only** PS/2 keyboard &
+mouse emulation.
+
+This also does not allow you to connect a PS/2 keyoard or mouse *to* a
+Raspberry Pi, it lets the Pi be the keyboard and mouse. This feature
+may be included in a future version though.
+
+fozztexx@fozztexx.com
+
+---
+
+See COPYING file for detailed information on terms of copying.
--- gpio.h
+++ gpio.h
@@ -1,3 +1,22 @@
+/* Copyright 2021 by Chris Osborn <fozztexx@fozztexx.com>
+ *
+ * This file is part of twokii.
+ *
+ * twokii is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * twokii is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with twokii; see the file COPYING. If not see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef GPIO_H
 #define GPIO_H 1
 
